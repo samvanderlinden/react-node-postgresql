1a8c5553f526b80a11b406cb01012478433d45f9
Sam Vanderlinden <samvanderlinden6@gmail.com>
2022-11-21 16:34:50 -0600

    add form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
+import React, { useState } from "react";
 import "./App.css";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/material/Button";
 
 function App() {
+  const [todoInput, setTodoInput] = useState("");
+
+  const onTodoInputChange = (e) => {
+    setTodoInput(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log({ todoInput });
+    setTodoInput("");
+  };
+
   return (
-    <div>
+    <div className="App">
       <h1>My ToDo App</h1>
+      <form onSubmit={onSubmitHandler}>
+        <FormControl>
+          <InputLabel htmlFor="todo-input">ToDo Input</InputLabel>
+          <Input
+            id="todo-input"
+            value={todoInput}
+            onChange={onTodoInputChange}
+          />
+        </FormControl>
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </form>
     </div>
   );
 }
