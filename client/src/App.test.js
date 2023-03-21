616fe5cfbc17d0efdf485ee01dfa2d6f0b521e5f
Sam Vanderlinden <samvanderlinden6@gmail.com>
2023-03-21 16:25:12 -0500

    adding test suite

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import { describe } from "@jest/globals";
 import App from "./App";
 
-test(`renders "My ToDo app"`, () => {
-  render(<App />);
-  const linkElement = screen.getByText(/My ToDo App/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test(`renders "My ToDo app"`, () => {
+    render(<App />);
+    const linkElement = screen.getByText(/My ToDo App/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
